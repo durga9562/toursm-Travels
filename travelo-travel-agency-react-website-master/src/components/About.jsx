b145c676d756dcd36be0dfe29052fa7d044516da
Durga Prasad Donga <dp21422@corp.ojas-it.com>
2022-09-12 14:15:31 +0530

    react

---
+++ travelo-travel-agency-react-website-master/src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./About.css";
+
+function About() {
+  return (
+    <div id="about">
+      <h1><strong>Title :  </strong>Manager Of The Project</h1>
+      <h3><strong>Name :  </strong>Nagaraju Madikanti</h3>
+      <h3><strong>Mobile : </strong>+91 9951766375</h3>
+      <h3><strong>Email : </strong>nagaraju.madikanti@ojas-it.com</h3>
+
+    </div>
+  )
+}
+
+export default About
